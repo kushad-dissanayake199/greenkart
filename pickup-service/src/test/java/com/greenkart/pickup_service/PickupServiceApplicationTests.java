087c24d7cfc4fd927ba8c17311456bbe005275e7
Kushad <kushad.dissanayake@axiatadigitallabs.com>
2025-06-03 03:08:38 -0300

    Initial commit

---
+++ pickup-service/src/test/java/com/greenkart/pickup_service/PickupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenkart.pickup_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
