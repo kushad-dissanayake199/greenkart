817ea38f8130a98914d23c71a102051272e16a38
Kushad <kushad.dissanayake@axiatadigitallabs.com>
2025-06-03 03:17:03 -0300

    Registration and logging add to user service.

---
+++ user-service/src/main/java/com/greenkart/user_service/model/User.java
@@ -0,0 +1,15 @@
+package com.greenkart.user_service.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table("users")
+public class User {
+    @Id
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+}
